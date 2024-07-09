8ad4ba50a90f833d9b74f62f5072b9d9d42b666c
Nayer Nagi <nayer-nagi@hotmail.com>
2024-07-09 20:20:34 +0300

    Add database configuration and dependency injection middleware

--- SMS.API/Program.cs
+++ SMS.API/Program.cs
@@ -1,3 +1,5 @@
+using SMS.Infrastructure.Data.Contexts;
+
 namespace SMS.API
 {
 	public class Program
@@ -7,8 +9,8 @@ namespace SMS.API
 			var builder = WebApplication.CreateBuilder(args);
 
 			// Add services to the container.
-
-			builder.Services.AddControllers();
+			builder.Services.AddDbContext<SMSDBContext>();
+            builder.Services.AddControllers();
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
