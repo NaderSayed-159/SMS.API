39efa6d6c78b88f46cf3b86a1fa3b4f09602bc7b
Nayer Nagi <nayer-nagi@hotmail.com>
2024-07-09 20:14:13 +0300

    Updating the ProductConfiguration: Adding The Table Schema Adding the realation One to Many With "Warehouse" Setting the Required Property to True for Fields : Name InternalCode StockAmount

--- SMS.Infrastructure.Data.Contexts/Configuration/ProductConfiguration.cs
+++ SMS.Infrastructure.Data.Contexts/Configuration/ProductConfiguration.cs
@@ -1,23 +1,39 @@
-﻿//using Microsoft.EntityFrameworkCore.Metadata.Builders;
-//using SMS.Domain.Entities;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SMS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace SMS.Infrastructure.Data.Contexts.Configuration
-//{
-//	public class ProductConfiguration: EntityTypeConfigurationBase<Product , Guid>
-//	{
-//		public override void Configure(EntityTypeBuilder<Product> builder)
-//		{
+namespace SMS.Infrastructure.Data.Contexts.Configuration
+{
+	public class ProductConfiguration : EntityTypeConfigurationBase<Product, Guid>
+	{
+		public override void Configure(EntityTypeBuilder<Product> builder)
+		{
+			builder
+				.ToTable("Products", "itemization");
 
-//			builder.ToTable ("Products", "dbo");
+            builder
+                .HasOne(p => p.Warehouse)
+                .WithMany(W => W.Products)
+                .HasForeignKey(p => p.WarehouseId);
 
+            builder
+				.Property(p => p.Name)
+				.IsRequired(true);
 
+            builder
+                .Property(p => p.InternalCode)
+                .IsRequired(true);
 
-//			base.Configure(builder);
-//		}
-//	}
-//}
+            builder
+                .Property(p => p.StockAmount)
+                .IsRequired(true);
+
+            base.Configure(builder);
+		}
+	}
+}
