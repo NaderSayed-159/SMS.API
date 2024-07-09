8ad581f6e16b75def786fde4203888e07dc38273
Nayer Nagi <nayer-nagi@hotmail.com>
2024-07-09 20:17:56 +0300

    Creating the Database

---
+++ SMS.Infrastructure.Data.Contexts/Migrations/20240707201528_initalmigration01.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SMS.Infrastructure.Data.Contexts.Migrations
+{
+    public partial class initalmigration01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "sales");
+
+            migrationBuilder.EnsureSchema(
+                name: "itemization");
+
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                schema: "sales",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Customerdepit = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Warehouse",
+                schema: "itemization",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Warehouse", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Invoice",
+                schema: "sales",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Serial = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    InvoiceDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invoice", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Invoice_Customer_CustomerId",
+                        column: x => x.CustomerId,
+                        principalSchema: "sales",
+                        principalTable: "Customer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                schema: "itemization",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    InternalCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WarehouseId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StockAmount = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Warehouse_WarehouseId",
+                        column: x => x.WarehouseId,
+                        principalSchema: "itemization",
+                        principalTable: "Warehouse",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerPayment",
+                schema: "sales",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PaiedAmount = table.Column<double>(type: "float", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    InvoiceId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerPayment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CustomerPayment_Customer_CustomerId",
+                        column: x => x.CustomerId,
+                        principalSchema: "sales",
+                        principalTable: "Customer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CustomerPayment_Invoice_InvoiceId",
+                        column: x => x.InvoiceId,
+                        principalSchema: "sales",
+                        principalTable: "Invoice",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InvoiceLine",
+                schema: "sales",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    productId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    TotalPrice = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InvoiceLine", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InvoiceLine_Products_productId",
+                        column: x => x.productId,
+                        principalSchema: "itemization",
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerPayment_CustomerId",
+                schema: "sales",
+                table: "CustomerPayment",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerPayment_InvoiceId",
+                schema: "sales",
+                table: "CustomerPayment",
+                column: "InvoiceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoice_CustomerId",
+                schema: "sales",
+                table: "Invoice",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InvoiceLine_productId",
+                schema: "sales",
+                table: "InvoiceLine",
+                column: "productId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_WarehouseId",
+                schema: "itemization",
+                table: "Products",
+                column: "WarehouseId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerPayment",
+                schema: "sales");
+
+            migrationBuilder.DropTable(
+                name: "InvoiceLine",
+                schema: "sales");
+
+            migrationBuilder.DropTable(
+                name: "Invoice",
+                schema: "sales");
+
+            migrationBuilder.DropTable(
+                name: "Products",
+                schema: "itemization");
+
+            migrationBuilder.DropTable(
+                name: "Customer",
+                schema: "sales");
+
+            migrationBuilder.DropTable(
+                name: "Warehouse",
+                schema: "itemization");
+        }
+    }
+}
---
+++ SMS.Infrastructure.Data.Contexts/SMSDBContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SMS.Domain.Entities;
+using SMS.Infrastructure.Data.Contexts.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Infrastructure.Data.Contexts
+{
+    public class SMSDBContext:DbContext
+    {
+        public SMSDBContext():base() { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=NERO\\SQLEXPRESS;Initial Catalog=SMS;User ID=sa;Password=12345;Connect Timeout=30;Encrypt=False;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            base.OnConfiguring(optionsBuilder);
+        }
+        public virtual DbSet<Customer> Customer { get; set; }
+        public virtual DbSet<CustomerPayment> CustomerPayment { get; set; }
+        public virtual DbSet<Invoice> Invoice { get; set; }
+        public virtual DbSet<InvoiceLine> InvoiceLine { get; set; }
+        public virtual DbSet<Product> Product { get; set; }
+        //public virtual DbSet<Purchaseorder> Purchaseorder { get; set; }
+        //public virtual DbSet<PurchasingExpenses> PurchasingExpenses { get; set; }
+        //public virtual DbSet<Vendor> Vendor { get; set; }
+        public virtual DbSet<Warehouse> Warehouse { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CustomerConfiguration());
+            modelBuilder.ApplyConfiguration(new CustomerPaymentConfiguration());
+            modelBuilder.ApplyConfiguration(new InvoiceConfiguration());
+            modelBuilder.ApplyConfiguration(new InvoiceLineConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new WarehouseConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
