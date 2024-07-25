218b2c869ef0832a7f1791db6f94879046639091
Nayer Nagi <nayer-nagi@hotmail.com>
2024-07-25 15:30:50 +0300

    Adding the Automapper profile

---
+++ SMS.Infrastructure.Business.AutoMapper/MapperProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using SMS.Domain.Entities;
+using SMS.Infrastructure.Model;
+
+namespace SMS.Infrastructure.Business.AutoMapper
+{
+    public class MapperProfile:Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Customer, CustomerModel>().ReverseMap();
+            CreateMap<CustomerPayment, CustomerPaymentModel>().ReverseMap();
+            CreateMap<Invoice, InvoiceModel>().ReverseMap();
+            CreateMap<InvoiceLine, InvoiceLineModel>().ReverseMap();
+            CreateMap<Product, ProductModel>().ReverseMap();
+            CreateMap<Purchaseorder, PurchaseorderModel>().ReverseMap();
+            CreateMap<PurchasingExpenses, PurchasingExpensesModel>().ReverseMap();
+            CreateMap<Vendor, VendorModel>().ReverseMap();
+            CreateMap<Warehouse, WarehouseModel>().ReverseMap();
+        }
+    }
+}
