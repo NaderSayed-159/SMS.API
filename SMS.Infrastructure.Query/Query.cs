5cfb41123c1eec1c5e677d1915556938086672f9
Nayer Nagi <nayer-nagi@hotmail.com>
2024-07-25 15:35:18 +0300

    Adding the Query.cs

---
+++ SMS.Infrastructure.Query/Query.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SMS.Domain.Entities;
+using SMS.Domain.Patterns.Interfaces;
+
+namespace SMS.Infrastructure.Query
+{
+    public static class Query
+    {
+        public static Task<TEntity> SelectByIdAsync<TEntity, TKey>(this IRepository<TEntity> repository, TKey id) where TEntity : EntityBase<TKey>
+        {
+            return repository
+                .Queryable()
+                .FirstOrDefaultAsync(q => q.Id.Equals(id));
+        }
+
+        public static Task<IEnumerable<TEntity>> SelectAsync<TEntity, TKey>(this IRepository<TEntity> repository) where TEntity : EntityBase<TKey>
+        {
+            return repository
+                .Query()
+                .SelectAsync();
+        }
+    }
+}
