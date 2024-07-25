fc46a44b9b6eae788c71dccd9960c73229f71e24
Nayer Nagi <nayer-nagi@hotmail.com>
2024-07-25 15:33:04 +0300

    Adding the Services to the dependency injection middleware

--- SMS.API/Program.cs
+++ SMS.API/Program.cs
@@ -1,3 +1,11 @@
+using AutoMapper;
+using eSealClientSample.Domain.Patterns.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SMS.Domain.Entities;
+using SMS.Domain.Patterns.Interfaces;
+using SMS.Infrastructure.Business;
+using SMS.Infrastructure.Business.AutoMapper;
 using SMS.Infrastructure.Data.Contexts;
 
 namespace SMS.API
@@ -10,6 +18,22 @@ namespace SMS.API
 
 			// Add services to the container.
 			builder.Services.AddDbContext<SMSDBContext>();
+
+			builder.Services
+                .AddScoped<IUnitOfWork, UnitOfWork>()
+                .AddScoped<ICustomerService, CustomerService>()
+				.AddScoped<ICustomerPaymentService, CustomerPaymentService>()
+				.AddScoped<IInvoiceLineService, InvoiceLineService>()
+				.AddScoped<IInvoiceService, InvoiceService>()
+				.AddScoped<IProductService, ProductService>()
+				.AddScoped<IPurchaseorderService, PurchaseorderService>()
+				.AddScoped<IPurchasingExpensesService, PurchasingExpensesService>()
+				.AddScoped<IVendorService, VendorService>()
+				.AddScoped<IWarehouseService, WarehouseService>();
+
+			builder.Services
+				.AddAutoMapper(typeof(MapperProfile));
+
             builder.Services.AddControllers();
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();
